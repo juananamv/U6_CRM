b611904205ed1340efd86ea8321f428887b9b905
Juan Villarin <villjua96@gmail.com>
2020-11-04 13:41:58 -0600

    Installed active storage for image uploading for each customer

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Customer do
-  permit_params :full_name, :phone_number, :email, :notes
+  permit_params :full_name, :phone_number, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 end
